a7d9f179872f6641691c8a44dce7ce004bb32d3b
anicmarko <anic.it36.2022@gmail.com>
2026-03-02 18:47:55 +0100

    test: add  unit tests for loan service

---
+++ services/loan-service/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/index.js',
+    '!**/node_modules/**'
+  ],
+  testMatch: ['**/tests/**/*.test.js'],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './test-results',
+      outputName: 'junit.xml'
+    }]
+  ]
+};
