cd34a4be1aad41cf32eb0f77739e13979542a6df
anicmarko <anic.it36.2022@gmail.com>
2026-03-03 00:28:56 +0100

    feat: implement notification service

---
+++ services/notification-service/jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/index.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 60,
+      lines: 70,
+      statements: 70
+    }
+  },
+  testMatch: ['**/tests/**/*.test.js'],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './test-results',
+      outputName: 'junit.xml'
+    }]
+  ]
+};
